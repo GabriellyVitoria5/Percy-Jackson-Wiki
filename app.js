1ba8ee75f7be78173d808a4d71cded404c60d5d4
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-09-07 18:36:09 -0300

    criando função de pesquisa, os dados só aparecem por meio da pesquisa

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 function pesquisar() {
     
     let section = document.getElementById("resultados-pesquisa");
-    let resultados = "";
+    let campoPesquisa = (document.getElementById("campo-pesquisa").value).toLowerCase()
 
-    // criar um novo elemento HTML para cada resultado
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Informe o nome de algum livro.</p>"
+        return 
+    }
+
+    let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let dataLancamento = "";
+
+    // mostra o livro encontrado se algum dado da pesquisa corresponde a esse livro
     for (let dado of dados) {
-        resultados += `
+
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        dataLancamento = dado.dataLancamento.toString().toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || dataLancamento.includes(campoPesquisa)) {
+            
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
                 </h2>
                 <p class="descricao-meta">${dado.descricao}</p>
-                <p class="descricao-meta">Data de lançamento: ${formatarData(dado.dataLancamento)}</p>
+                <p class="descricao-meta">${formatarData(dado.dataLancamento)}</p>
             </div>
         `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nenhum livro foi encontrado</p>"
     }
 
     section.innerHTML = resultados;
