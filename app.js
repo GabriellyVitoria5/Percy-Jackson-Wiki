e3f0215b7e8e514de22c2470313bbdc7e0a546d1
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-09-07 17:59:51 -0300

    mostrando todos os dados do arquivo dados.js na tela

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log(dados)
+function pesquisar() {
+    
+    let section = document.getElementById("resultados-pesquisa");
+    let resultados = "";
+
+    // criar um novo elemento HTML para cada resultado
+    for (let dado of dados) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <p>${dado.dataLancamento}</p>
+            </div>
+        `;
+    }
+
+    section.innerHTML = resultados;
+}
