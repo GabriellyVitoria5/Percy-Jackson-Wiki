402896c11135d058f23b43fe22d722d25d0bf605
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-09-07 22:07:55 -0300

    corrigindo caminho das imagens

--- dados.js
+++ dados.js
@@ -9,54 +9,54 @@ let dados = [
       titulo: "O Mar de Monstros",
       descricao: "Percy e seus amigos devem encontrar o Velocino de Ouro para salvar o acampamento meio-sangue.",
       dataLancamento: new Date(2006, 8, 1), // 1º de setembro de 2006
-      img: "img/2_mar_monstros"
+      img: "img/2_mar_monstros.jpeg"
     },
     {
       titulo: "A Maldição do Titã",
       descricao: "Percy e Annabeth são acusados de um crime que não cometeram e devem provar sua inocência.",
       dataLancamento: new Date(2007, 7, 1), // 1º de agosto de 2007
-      img: "img/3_maldicao_tita"
+      img: "img/3_maldicao_tita.jpeg"
     },
     {
       titulo: "A Batalha do Labirinto",
       descricao: "Percy e seus amigos devem encontrar Nico di Angelo e impedir a invasão do acampamento.",
       dataLancamento: new Date(2008, 4, 6), // 6 de maio de 2008
-      img: "img/4_batalha_labirinto"
+      img: "img/4_batalha_labirinto.jpeg"
     },
     {
       titulo: "O Último Olimpiano",
       descricao: "A batalha final entre os deuses e os titãs está prestes a começar.",
       dataLancamento: new Date(2009, 4, 5), // 5 de maio de 2009
-      img: "img/5_ultimo_olimpiano"
+      img: "img/5_ultimo_olimpiano.jpeg"
     },
     {
       titulo: "O Herói Perdido",
       descricao: "Percy, Annabeth e Grover embarcam em uma jornada para encontrar Nico di Angelo e impedir a ressurreição de Cronos.",
       dataLancamento: new Date(2009, 11, 12),
-      img: "img/6_heroi_perdido"
+      img: "img/6_heroi_perdido.jpeg"
   },
   {
       titulo: "O Filho de Netuno",
       descricao: "Percy acorda sem memória em Roma e descobre que é um legado de Poseidon.",
       dataLancamento: new Date(2011, 4, 5),
-      img: "img/7_filho_netuno"
+      img: "img/7_filho_netuno.jpeg"
   },
   {
       titulo: "A Marca de Atena",
       descricao: "Annabeth Chase é sequestrada e Percy e seus amigos devem resgatá-la.",
       dataLancamento: new Date(2012, 4, 4),
-      img: "img/8_marca_atena"
+      img: "img/8_marca_atena.jpeg"
   },
   {
       titulo: "A Casa de Hades",
       descricao: "Percy e Annabeth viajam para o submundo para fechar a Porta da Morte.",
       dataLancamento: new Date(2013, 4, 3),
-      img: "img/9_casa_hades"
+      img: "img/9_casa_hades.jpeg"
   },
   {
       titulo: "O Sangue do Olimpo",
       descricao: "A batalha final entre os deuses e os gigantes se aproxima.",
       dataLancamento: new Date(2014, 4, 2),
-      img: "img/10_sangue_olimpo"
+      img: "img/10_sangue_olimpo.jpeg"
   }
   ];
